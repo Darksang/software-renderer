a42bf2912184f2038ce537b161bae2b2adcdae27
Darksang <darksang.exodar@gmail.com>
2018-11-23 18:51:51 +0100

    Added basic vectors

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 #include <SDL.h>
 
+#include "Vector2i.h"
+#include "Vector3f.h"
+
 int main(int argc, char ** argv) {
     
     SDL_Init(SDL_INIT_VIDEO);
+
+    Vector2i v0(2, 3);
+    Vector3f v1(3, 5, 8);
+
+    std::cout << "x: " << v0.x << " y: " << v0.y << std::endl;
+    std::cout << "x: " << v1.x << " y: " << v1.y << " z: " << v1.z << std::endl;
+
+    v1 *= 2;
+
+    std::cout << "x: " << v1.x << " y: " << v1.y << " z: " << v1.z << std::endl;
+
+    Vector3f v2(7, 2, 8);
+
+    float dot = Dot(v1, v2);
+
+    std::cout << "Dot Product v1/v2: " << dot << std::endl;
 
     SDL_Window * Window = SDL_CreateWindow("Testing", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
 
