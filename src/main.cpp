1004011c63f5ee4c542bd535375e2819021116a1
Darksang <darksang.exodar@gmail.com>
2019-04-10 13:20:32 +0200

    Got rid of buffers as std vector, was too slow

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,15 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-void DrawLine(glm::vec2 p0, glm::vec2 p1, std::vector<glm::vec4> & FrameBuffer, const glm::vec4 Color) {
+struct Pixel {
+    unsigned char b;
+    unsigned char g;
+    unsigned char r;
+    unsigned char a;
+};
+
+void DrawLine(glm::vec2 p0, glm::vec2 p1, Pixel Framebuffer[], glm::vec4 Color) {
+    Color = glm::clamp(Color, 0.0f, 255.0f);
     // Check if we need to reverse input coordinates
     bool Steep = false;
 
@@ -33,10 +41,17 @@ void DrawLine(glm::vec2 p0, glm::vec2 p1, std::vector<glm::vec4> & FrameBuffer, 
     int y = p0.y;
 
     for (int x = p0.x; x <= p1.x; x++) {
-        if (Steep)
-            FrameBuffer[x * SCREEN_WIDTH + y] = Color;
-        else
-            FrameBuffer[y * SCREEN_WIDTH + x] = Color;
+        if (Steep) {
+            Framebuffer[x * SCREEN_WIDTH + y].a = static_cast<unsigned char>(Color.w);
+            Framebuffer[x * SCREEN_WIDTH + y].r = static_cast<unsigned char>(Color.x);
+            Framebuffer[x * SCREEN_WIDTH + y].g = static_cast<unsigned char>(Color.y);
+            Framebuffer[x * SCREEN_WIDTH + y].b = static_cast<unsigned char>(Color.z);
+        } else {
+            Framebuffer[y * SCREEN_WIDTH + x].a = static_cast<unsigned char>(Color.w);
+            Framebuffer[y * SCREEN_WIDTH + x].r = static_cast<unsigned char>(Color.x);
+            Framebuffer[y * SCREEN_WIDTH + x].g = static_cast<unsigned char>(Color.y);
+            Framebuffer[y * SCREEN_WIDTH + x].b = static_cast<unsigned char>(Color.z);
+        }
 
         error += m;
 
@@ -57,22 +72,17 @@ int main(int argc, char **argv) {
 
     SDL_Event Event;
 
-    std::vector<unsigned char> ColorBuffer;
-    std::vector<glm::vec4> FrameBuffer;
-
-    ColorBuffer.resize(SCREEN_WIDTH * SCREEN_HEIGHT * 4);
-    FrameBuffer.resize(SCREEN_WIDTH * SCREEN_HEIGHT);
-
-    std::vector<glm::vec4> AuxFrameBuffer;
-    AuxFrameBuffer.resize(SCREEN_WIDTH * SCREEN_HEIGHT);
     glm::vec4 ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    std::fill(AuxFrameBuffer.begin(), AuxFrameBuffer.end(), ClearColor);
 
-    int FrameBufferDimensions = SCREEN_WIDTH * SCREEN_HEIGHT * 4;
+    // Initialize required buffers
+    Pixel * FrameBuffer = new Pixel[SCREEN_WIDTH * SCREEN_HEIGHT];
+    memset(FrameBuffer, 0, (SCREEN_WIDTH * SCREEN_HEIGHT) * sizeof(Pixel));
 
     bool Running = true;
 
     while (Running) {
+        const auto FrameBegin = std::chrono::high_resolution_clock::now();
+
         // Process events
         while (SDL_PollEvent(&Event)) {
             switch (Event.type) {
@@ -104,53 +114,29 @@ int main(int argc, char **argv) {
         }
 
         // Clear screen
-        SDL_SetRenderDrawColor(Renderer, (uint8_t)ClearColor.x, (uint8_t)ClearColor.y, (uint8_t)ClearColor.z, SDL_ALPHA_OPAQUE);
+        SDL_SetRenderDrawColor(Renderer, static_cast<uint8_t>(ClearColor.x), static_cast<uint8_t>(ClearColor.y), static_cast<uint8_t>(ClearColor.z), SDL_ALPHA_OPAQUE);
         SDL_RenderClear(Renderer);
 
-        // Invalidate buffer
-        const auto InvalidateBegin = std::chrono::high_resolution_clock::now();
-        FrameBuffer = AuxFrameBuffer;
-        const auto InvalidateEnd = std::chrono::high_resolution_clock::now();
-
-        std::cout << "Invalidate Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(InvalidateEnd - InvalidateBegin).count() << "ms" << std::endl;
+        // Invalidate buffers
+        memset(FrameBuffer, 0, (SCREEN_WIDTH * SCREEN_HEIGHT) * sizeof(Pixel));
 
         // Test line
-        const auto LineBegin = std::chrono::high_resolution_clock::now();
-        DrawLine(glm::vec2(0.0f, 0.0f), glm::vec2(320.0f, 240.0f), FrameBuffer, glm::vec4(0.239f, 0.952f, 0.070f, 1.0f));
-        const auto LineEnd = std::chrono::high_resolution_clock::now();
-
-        std::cout << "Line Draw Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(LineEnd - LineBegin).count() << "ms" << std::endl;
-
-        // Write to ColorBuffer
-        const auto WriteColorBegin = std::chrono::high_resolution_clock::now();
-        #pragma omp parallel for
-        for (int i = 0; i < FrameBufferDimensions; i += 4) {
-            glm::vec4 rgba = FrameBuffer[i / 4];
-            rgba = glm::clamp(rgba, 0.0f, 1.0f);
-
-            ColorBuffer[i] = (unsigned char)(255 * rgba.b);
-            ColorBuffer[i + 1] = (unsigned char)(255 * rgba.g);
-            ColorBuffer[i + 2] = (unsigned char)(255 * rgba.r);
-            ColorBuffer[i + 3] = (unsigned char)(255 * rgba.a);
-        }
-        const auto WriteColorEnd = std::chrono::high_resolution_clock::now();
-
-        std::cout << "Write Color Buffer Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(WriteColorEnd - WriteColorBegin).count() << "ms" << std::endl;
+        DrawLine(glm::vec2(0.0f, 0.0f), glm::vec2(320.0f, 240.0f), FrameBuffer, glm::vec4(0, 0, 255, 0.1f));
+        DrawLine(glm::vec2(30.0f, 235.0f), glm::vec2(30.0f, 298.0f), FrameBuffer, glm::vec4(0, 255, 0, 0.1f));
 
         // Display frame
-        const auto PresentBegin = std::chrono::high_resolution_clock::now();
-        SDL_UpdateTexture(Texture, nullptr, ColorBuffer.data(), SCREEN_WIDTH * 4);
+        SDL_UpdateTexture(Texture, nullptr, FrameBuffer, SCREEN_WIDTH * 4);
         SDL_RenderCopy(Renderer, Texture, nullptr, nullptr);
 
         SDL_RenderPresent(Renderer);
-        const auto PresentEnd = std::chrono::high_resolution_clock::now();
 
-        std::cout << "Present Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(PresentEnd - PresentBegin).count() << "ms" << std::endl << std::endl;
+        /* const auto FrameEnd = std::chrono::high_resolution_clock::now();
+        std::cout << "Frame Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(FrameEnd - FrameBegin).count() << "ms";
+        auto FramesPerSecond = 1000.0f / std::chrono::duration_cast<std::chrono::milliseconds>(FrameEnd - FrameBegin).count();
+        std::cout << " FPS: " << (int)FramesPerSecond << std::endl; */
     }
 
-    ColorBuffer.clear();
-    FrameBuffer.clear();
-    AuxFrameBuffer.clear();
+    delete [] FrameBuffer;
 
     SDL_DestroyTexture(Texture);
     SDL_DestroyRenderer(Renderer);
