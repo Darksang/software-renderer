0e18d9fd2864a1e8fc0e9f15ca18ebecad665512
Darksang <darksang.exodar@gmail.com>
2019-04-14 20:47:03 +0200

    Real time frame display in progress

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,22 @@
 
 #include <glm/glm.hpp>
 
-#include <vector>
 #include <algorithm>
 #include <chrono>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+static const int SCREEN_WIDTH = 640;
+static const int SCREEN_HEIGHT = 480;
 
 struct Pixel {
-    unsigned char b;
-    unsigned char g;
-    unsigned char r;
-    unsigned char a;
-};
+    uint8_t b;
+    uint8_t g;
+    uint8_t r;
+    uint8_t a;
+}; 
 
-void DrawLine(glm::vec2 p0, glm::vec2 p1, Pixel Framebuffer[], glm::vec4 Color) {
+static Pixel * FrameBuffer;
+
+void DrawLine(glm::vec2 p0, glm::vec2 p1, glm::vec4 Color) {
     Color = glm::clamp(Color, 0.0f, 255.0f);
     // Check if we need to reverse input coordinates
     bool Steep = false;
@@ -42,15 +43,15 @@ void DrawLine(glm::vec2 p0, glm::vec2 p1, Pixel Framebuffer[], glm::vec4 Color) 
 
     for (int x = p0.x; x <= p1.x; x++) {
         if (Steep) {
-            Framebuffer[x * SCREEN_WIDTH + y].a = static_cast<unsigned char>(Color.w);
-            Framebuffer[x * SCREEN_WIDTH + y].r = static_cast<unsigned char>(Color.x);
-            Framebuffer[x * SCREEN_WIDTH + y].g = static_cast<unsigned char>(Color.y);
-            Framebuffer[x * SCREEN_WIDTH + y].b = static_cast<unsigned char>(Color.z);
+            FrameBuffer[x * SCREEN_WIDTH + y].a = static_cast<uint8_t>(Color.w);
+            FrameBuffer[x * SCREEN_WIDTH + y].r = static_cast<uint8_t>(Color.x);
+            FrameBuffer[x * SCREEN_WIDTH + y].g = static_cast<uint8_t>(Color.y);
+            FrameBuffer[x * SCREEN_WIDTH + y].b = static_cast<uint8_t>(Color.z);
         } else {
-            Framebuffer[y * SCREEN_WIDTH + x].a = static_cast<unsigned char>(Color.w);
-            Framebuffer[y * SCREEN_WIDTH + x].r = static_cast<unsigned char>(Color.x);
-            Framebuffer[y * SCREEN_WIDTH + x].g = static_cast<unsigned char>(Color.y);
-            Framebuffer[y * SCREEN_WIDTH + x].b = static_cast<unsigned char>(Color.z);
+            FrameBuffer[y * SCREEN_WIDTH + x].a = static_cast<uint8_t>(Color.w);
+            FrameBuffer[y * SCREEN_WIDTH + x].r = static_cast<uint8_t>(Color.x);
+            FrameBuffer[y * SCREEN_WIDTH + x].g = static_cast<uint8_t>(Color.y);
+            FrameBuffer[y * SCREEN_WIDTH + x].b = static_cast<uint8_t>(Color.z);
         }
 
         error += m;
@@ -63,22 +64,20 @@ void DrawLine(glm::vec2 p0, glm::vec2 p1, Pixel Framebuffer[], glm::vec4 Color) 
 }
 
 int main(int argc, char **argv) {
-
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window * Window = SDL_CreateWindow("Software Renderer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     SDL_Renderer * Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Texture * Texture = SDL_CreateTexture(Renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_TARGET, SCREEN_WIDTH, SCREEN_HEIGHT);
+    SDL_Texture * Texture = SDL_CreateTexture(Renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     SDL_Event Event;
 
-    glm::vec4 ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
     // Initialize required buffers
-    Pixel * FrameBuffer = new Pixel[SCREEN_WIDTH * SCREEN_HEIGHT];
+    FrameBuffer = new Pixel[SCREEN_WIDTH * SCREEN_HEIGHT];
     memset(FrameBuffer, 0, (SCREEN_WIDTH * SCREEN_HEIGHT) * sizeof(Pixel));
 
     bool Running = true;
+    bool Swap = true;
 
     while (Running) {
         const auto FrameBegin = std::chrono::high_resolution_clock::now();
@@ -93,7 +92,13 @@ int main(int argc, char **argv) {
                     break;
 
                 case SDL_SCANCODE_SPACE:
-                    std::cout << "Space Pressed" << std::endl;
+                    Swap = !Swap;
+
+                    if (Swap)
+                        std::cout << "Swap True, Draw Green Line" << std::endl;
+                    else
+                        std::cout << "Swap False, Draw Blue Line" << std::endl;
+
                     break;
 
                 default:
@@ -113,27 +118,23 @@ int main(int argc, char **argv) {
             }
         }
 
-        // Clear screen
-        SDL_SetRenderDrawColor(Renderer, static_cast<uint8_t>(ClearColor.x), static_cast<uint8_t>(ClearColor.y), static_cast<uint8_t>(ClearColor.z), SDL_ALPHA_OPAQUE);
-        SDL_RenderClear(Renderer);
-
         // Invalidate buffers
         memset(FrameBuffer, 0, (SCREEN_WIDTH * SCREEN_HEIGHT) * sizeof(Pixel));
 
         // Test line
-        DrawLine(glm::vec2(0.0f, 0.0f), glm::vec2(320.0f, 240.0f), FrameBuffer, glm::vec4(0, 0, 255, 0.1f));
-        DrawLine(glm::vec2(30.0f, 235.0f), glm::vec2(30.0f, 298.0f), FrameBuffer, glm::vec4(0, 255, 0, 0.1f));
+        if (Swap == true)
+            DrawLine(glm::vec2(30.0f, 235.0f), glm::vec2(30.0f, 298.0f), glm::vec4(0, 255, 0, 255));
+        else 
+            DrawLine(glm::vec2(0.0f, 0.0f), glm::vec2(640.0f, 480.0f), glm::vec4(0, 0, 255, 255));
 
         // Display frame
-        SDL_UpdateTexture(Texture, nullptr, FrameBuffer, SCREEN_WIDTH * 4);
-        SDL_RenderCopy(Renderer, Texture, nullptr, nullptr);
+        if (SDL_UpdateTexture(Texture, nullptr, FrameBuffer, SCREEN_WIDTH * sizeof(Pixel)) < 0)
+            std::cout << "Update Texture Error" << std::endl << SDL_GetError() << std::endl;
+
+        if (SDL_RenderCopy(Renderer, Texture, nullptr, nullptr) < 0)
+            std::cout << "Render Copy Error" << std::endl << SDL_GetError() << std::endl;
 
         SDL_RenderPresent(Renderer);
-
-        /* const auto FrameEnd = std::chrono::high_resolution_clock::now();
-        std::cout << "Frame Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(FrameEnd - FrameBegin).count() << "ms";
-        auto FramesPerSecond = 1000.0f / std::chrono::duration_cast<std::chrono::milliseconds>(FrameEnd - FrameBegin).count();
-        std::cout << " FPS: " << (int)FramesPerSecond << std::endl; */
     }
 
     delete [] FrameBuffer;
